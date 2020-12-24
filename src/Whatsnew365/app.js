48bd99ff7da71b6a1bd8a69c775cfad1a8974209
andrewta999 <andrewta365@gmail.com>
2020-12-24 12:05:04 -0500

    add routes to the app

--- src/Whatsnew365/app.js
+++ src/Whatsnew365/app.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 
+import stream_router from './route/index'
+
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/api', stream_router)
 
 app.get('/', (req, res) => {
     res.send("Hello")
