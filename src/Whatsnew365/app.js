90fddd492f3c642d8cffe6b95dc5e2c5ba1f1cee
andrewta999 <andrewta365@gmail.com>
2020-12-24 12:12:43 -0500

    added cors support for the app

--- src/Whatsnew365/app.js
+++ src/Whatsnew365/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 import bodyParser from 'body-parser'
 
 import stream_router from './route/index'
 
 const app = express()
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
