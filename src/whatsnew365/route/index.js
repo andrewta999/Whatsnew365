13be18205016c4e1a391d84f3fce8696d22c35d7
andrewta999 <andrewta365@gmail.com>
2020-12-26 12:27:04 -0500

    some minor name changes

--- src/whatsnew365/route/index.js
+++ src/whatsnew365/route/index.js
@@ -1,17 +1,17 @@
 import express from 'express'
 import {get_rules_api, set_rule_api, 
-        delete_a_rule, delete_all_rules, 
-        start_streaming, stop_streaming} from '../controller/index'
+        delete_a_rule_api, delete_all_rules_api, 
+        start_streaming_api, stop_streaming_api} from '../controller/index'
 
 const router = express.Router()
 
 router.get('/rules', get_rules_api)
 router.post('/rules', set_rule_api)
 
-router.post('/delete', delete_a_rule)
-router.get('/delete/all', delete_all_rules)
+router.post('/delete', delete_a_rule_api)
+router.get('/delete/all', delete_all_rules_api)
 
-router.get('/stream', start_streaming)
-router.get('/stop', stop_streaming)
+router.get('/stream', start_streaming_api)
+router.get('/stop', stop_streaming_api)
 
 export default router 
