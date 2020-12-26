f9e70884973b26b0e6b41f097add8e95353e3735
andrewta999 <andrewta365@gmail.com>
2020-12-26 16:49:05 -0500

    initial code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import { io } from 'socket.io-client'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    // constructor(props){
+    //     super(props)
+    // }
+
+    componentDidMount(){
+        const socket = io('http://localhost:3001')
+        socket.on("connected", (message) => {
+            console.log(message)
+        })
+    }
+
+    render(){
+        return <div>
+            Hello App
+        </div>
+    }
 }
 
-export default App;
+
+export default App
