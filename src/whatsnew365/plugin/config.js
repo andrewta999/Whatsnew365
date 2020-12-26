d21724f7e2180860f1e0bd7991234915ed018143
andrewta999 <andrewta365@gmail.com>
2020-12-26 12:26:29 -0500

    added basic rule template for twitter filtered stream

--- src/whatsnew365/plugin/config.js
+++ src/whatsnew365/plugin/config.js
@@ -2,6 +2,6 @@ export default {
     token: 'AAAAAAAAAAAAAAAAAAAAAA3gKAEAAAAA2dnq%2FUwjaVlGLljIsBUh2NsnXOI%3DGdVALMVwklumsOUJHgpHw6JhGvhdNzrLTLODYMW7mUTCfBLZ6l',
     rules_url: "https://api.twitter.com/2/tweets/search/stream/rules",
     stream_url: `https://api.twitter.com/2/tweets/search/stream?tweet.fields=attachments,author_id,created_at,entities,geo,id,lang,possibly_sensitive,public_metrics,text&expansions=author_id,geo.place_id,attachments.media_keys`,
-    too_basic_rule: "lang:en",
-    basic_rule: "-is:retweet -is:reply lang:en",
+    too_basic_rule: "-is:retweet lang:en",
+    basic_rule: "-is:retweet -is:reply"
 }
