66ab6d55be0e138193a85b3347a97b960a662495
andrewta999 <andrewta365@gmail.com>
2020-12-26 12:25:52 -0500

    map controller functions to twitter plugin functions

--- src/whatsnew365/controller/index.js
+++ src/whatsnew365/controller/index.js
@@ -1,40 +1,67 @@
+import {
+    get_rules, 
+    set_rules, 
+    delete_all_rules, 
+    delete_a_rule, 
+    construct_rule, 
+    construct_rule_and_tag, 
+    start_stream
+} from '../plugin/twitter'
 
 
-function get_rules_api(req, res){
-    res.send("Get all rules api")
+let stream_instance
+
+
+async function get_rules_api(req, res){
+    let rules = await get_rules()
+    res.json(rules)
 }
 
 
-function set_rule_api(req, res){
-    res.send("Set a rule api")
+async function set_rule_api(req, res){
+    let data = req.body 
+    let {keyword, sample, language} = data 
+    let rule = construct_rule(keyword, sample, language)
+    let rule_array = [construct_rule_and_tag(rule, keyword)]
+    let response = await set_rules(rule_array)
+    res.json(response)
 }
 
 
-function delete_a_rule(req, res){
-    res.send("Delete a rules")
+async function delete_a_rule_api(req, res){
+    let {rule_id} = req.body
+    let response = await delete_a_rule(rule_id)
+    res.json(response)
+} 
+
+
+async function delete_all_rules_api(req, res){
+    let response = await delete_all_rules()
+    res.json(response)
 }
 
 
-function delete_all_rules(req, res){
-    res.send("Delete all rules")
+async function start_streaming_api(req, res){
+    stream_instance = await start_stream()
+    res.json("Stream started")
 }
 
 
-function start_streaming(req, res){
-    res.send("Start streaming")
-}
-
-
-function stop_streaming(req, res){
-    res.send("Stop streaming")
+function stop_streaming_api(req, res){
+    if (stream_instance){
+        stream_instance.destroy()
+        res.json("Stream stopped")
+    }else{
+        res.json("No stream is available")
+    }
 }
 
 
 export {
     get_rules_api, 
     set_rule_api, 
-    delete_a_rule, 
-    delete_all_rules, 
-    start_streaming, 
-    stop_streaming
+    delete_a_rule_api, 
+    delete_all_rules_api, 
+    start_streaming_api, 
+    stop_streaming_api
 }
