61631616d978f57f063c9f5e38f162ca5bf9fb83
andrewta999 <andrewta365@gmail.com>
2020-12-27 17:39:39 -0500

    removed some unnecessary code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,8 @@
 import React, {Component} from 'react'
-import { io } from 'socket.io-client'
 
 import Main from './component/Main'
 
 class App extends Component{
-    // componentDidMount(){
-    //     const socket = io('http://localhost:3001')
-    //     socket.on("connected", (message) => {
-    //         console.log(message)
-    //     })
-    // }
-
     render(){
         return <div className="container-fluid">
             <Main />
